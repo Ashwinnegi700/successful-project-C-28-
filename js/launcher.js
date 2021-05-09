57e140fc2bbed2cc50ded6020683c5210b100b89
Ashwinnegi700 <78861623+Ashwinnegi700@users.noreply.github.com>
2021-05-09 18:11:57 +0530

    Add files via upload

---
+++ js/launcher.js
@@ -0,0 +1,25 @@
+class Launcher{
+constructor(body,point){
+    var Option = {
+        bodyA:body,
+        pointB:point,
+        stiffness:0.004,
+        length:1
+    }
+   this.launcher=Constraint.create(Option);
+   World.add(world,this.launcher);
+}
+fly(){
+    this.launcher.bodyA=null
+}
+
+   display(){
+       if(this.launcher.bodyA){
+        strokeWeight(4)
+        line(this.launcher.bodyA.position.x,this.launcher.bodyA.position.y,
+         this.launcher.pointB.x,this.launcher.pointB.y);
+       }
+      
+   }
+
+}
